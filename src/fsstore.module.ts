895d2789568060af0a49a139c3be245ae70e39b3
Max Sergeev <sergeev.maxs@gmail.com>
2017-11-30 05:25:39 -0600

    initial commit

---
+++ src/fsstore.module.ts
@@ -0,0 +1,16 @@
+import { FsStore } from './fsstore.service';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+    ],
+    providers: [
+      FsStore
+    ],
+    exports: [
+
+    ]
+})
+export class FsStoreModule {
+
+}
