1f78e3e235b1f3d23390f2f31c470211241c0d56
Raymond Gigliotti <ray@firestitch.com>
2019-03-08 10:33:23 -0500

    7.0.0

--- playground/app/app.component.ts
+++ playground/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 
 @Component({
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  public config = environment;
 }
