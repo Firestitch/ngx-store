bf0a688d1d117cac09573545d6dac6bf1cb2462f
Eugene Kubesh <e.kubesh@wis.eu.com>
2020-02-04 16:16:39 +0300

    upgraded to n8

--- src/app/classes/store-object.ts
+++ src/app/classes/store-object.ts
@@ -3,5 +3,5 @@ export class FsStoreObject {
   static readonly EVENT_REMOVE = 'remove';
   static readonly EVENT_INIT = 'init';
 
-  constructor(private name, private event, private value?) {}
+  constructor(public name, public event, public value?) {}
 }
